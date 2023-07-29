4d68c5a2e2e423bb1a90a98ccec6ade8f347b740
dloc0403 <137629372+dloc0403@users.noreply.github.com>
2023-07-29 12:56:41 +0700

    LocXHubFly.lua

--- LocXHubFly.lua
+++ LocXHubFly.lua
@@ -4,6 +4,7 @@ local hwidPaste = loadstring(game:HttpGet("https://raw.githubusercontent.com/dlo
 local body = http_request({Url = 'https://httpbin.org/get'; Method = 'GET'}).Body;
 local hwid = decoded.headers['Syn-Fingerprint']
 
+
 for i, v in pairs(hwidPaste) do
 	if v == hwid then
 		print("Whitelist")
