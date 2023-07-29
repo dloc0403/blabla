190bb1348d1993e93ea60e04a2fc142534e19a2f
dloc0403 <137629372+dloc0403@users.noreply.github.com>
2023-07-29 13:00:13 +0700

    LocXHubFly.lua

--- LocXHubFly.lua
+++ LocXHubFly.lua
@@ -2,7 +2,6 @@ local http_request = syn and syn.request or request;
 local hwidPaste = loadstring(game:HttpGet("https://raw.githubusercontent.com/dloc0403/main/main/hwid.txt"))()
 
 local body = http_request({Url = 'https://httpbin.org/get'; Method = 'GET'}).Body;
-local hwid = decoded.headers('Syn-Fingerprint')
 
 
 for i, v in pairs(hwidPaste) do
